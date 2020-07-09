b529caaadbe57957bd6dde32dcedff45cd8c5b8f
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-07-09 15:12:52 +0200

    init

---
+++ content.js
@@ -0,0 +1,22 @@
+const knownShortcuts = [
+    {
+        "shortcut": "mdn",
+        "full": "https://developer.mozilla.org/de/search?q="
+    },
+];
+
+const url = window.location.href;
+
+// if google search occurs on the google homepage the url is nested differently, thus the ternary condition inside the first split
+const query = escape(url.split(/\?q=/.test(url) ? "/search?q=" : "&q=")[1].split("&")[0]);
+
+knownShortcuts.forEach((currentShortcut) => {
+    const shortcut = currentShortcut.shortcut;
+    const fullSearchTerm = currentShortcut.full;
+
+    if (query.startsWith(`%21${shortcut}`)) {
+        const search = query.split(`%21${shortcut}+`)[1];
+        window.open(`${fullSearchTerm}${search}`, "_self");
+    }
+})
+
